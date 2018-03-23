945744c7a41dc021a8d85010ac4d427a0b634ec6
Josh <denman.joshua@yahoo.com>
2018-03-22 17:55:34 -0700

    Add project files.

---
+++ JoshsPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using JoshsPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JoshsPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Pie> Pies { get; set; }
+        public string Title { get; set; }
+
+    }
+}
